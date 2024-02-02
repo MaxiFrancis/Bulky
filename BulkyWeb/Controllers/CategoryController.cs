80bb5d5ca55029337cb58fb7917ba2a50a760efd
Francisc Suba <francisc.suba@octavic.ro>
2024-02-02 15:35:36 +0200

    2.51

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -63,6 +64,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -95,6 +97,7 @@ namespace BulkyWeb.Controllers
             }
                 _db.Categories.Remove(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
                 return RedirectToAction("Index");
         }
     }
