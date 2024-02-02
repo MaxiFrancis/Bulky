cbd33b5b0815c8da33287fc1615b37f8a34a1943
Francisc Suba <francisc.suba@octavic.ro>
2024-02-02 14:59:56 +0200

    2.46

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -24,8 +24,9 @@ namespace BulkyWeb.Controllers
 
         [HttpPost]
         public IActionResult Create(Category obj)
-        {   
-            if (obj.Name == obj.DisplayOrder.ToString()) {
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
                 ModelState.AddModelError("name", "The dysplay order cannot match the name!");
             }
             if (ModelState.IsValid)
@@ -37,5 +38,38 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            //Category categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The dysplay order cannot match the name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+        }
     }
 }
