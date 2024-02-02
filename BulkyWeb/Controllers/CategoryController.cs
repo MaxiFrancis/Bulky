1028b9c67d3e079269ca9532d77cfa3ad989b20f
Francisc Suba <francisc.suba@octavic.ro>
2024-02-02 13:49:53 +0200

    2.40

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,5 +21,14 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);   
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
