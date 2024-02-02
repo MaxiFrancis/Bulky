0278611fc13a1e6a71c745fecfdb0c3121f60cfd
Francisc Suba <francisc.suba@octavic.ro>
2024-02-02 13:40:11 +0200

    2.39

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,7 +8,9 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }    
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Dysplay Order")]
         public int DisplayOrder { get; set; }
         
     }
