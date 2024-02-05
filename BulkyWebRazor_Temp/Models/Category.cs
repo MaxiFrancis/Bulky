b86b2725a88045cdb2e38508f99229d3877f2291
Francisc Suba <francisc.suba@octavic.ro>
2024-02-05 10:41:24 +0200

    3.55

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Dysplay Order")]
+        [Range(1, 100, ErrorMessage = "The field Dysplay Order must be between 1 and 100.\r\n")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
