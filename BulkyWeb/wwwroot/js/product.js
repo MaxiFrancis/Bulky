b107bc387e9898437af2f75f19b29843522f9703
Francisc Suba <francisc.suba@octavic.ro>
2024-02-27 14:50:51 +0200

    102

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -1,17 +1,17 @@
 ﻿
 
 $(document).ready(function () {
-    localDataTable();
+    loadDataTable();
 });
 
 function loadDataTable() {
-    dataTable = $('#tblTable').DataTable({
+    dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { data: 'title', "width":"15%"},
+            { data: 'title', "width": "25%" },
             { data: 'isbn', "width": "15%" },
-            { data: 'listPrice', "width": "15%" },
-            { data: 'author', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'author', "width": "20%" },
             { data: 'category.name', "width": "15%" }
         ]
 
