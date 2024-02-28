7647cf78bc1d88c54916c58ad4e69cbcd5b0614f
Francisc Suba <francisc.suba@octavic.ro>
2024-02-28 13:41:37 +0200

    7.109

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
         public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u=>u.Id == productId, includeProperties: "Category");
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
             return View(product);
         }
 
