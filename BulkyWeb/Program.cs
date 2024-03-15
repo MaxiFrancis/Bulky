1182b0b85a1040fa8abefea0bd80591686ec98c0
Francisc Suba <francisc.suba@octavic.ro>
2024-03-15 15:06:57 +0200

    14

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -46,11 +46,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
