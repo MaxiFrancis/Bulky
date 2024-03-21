59d87a410eae2ea4dbd91c7a4e0ae32e16e65984
Francisc Suba <francisc.suba@octavic.ro>
2024-03-21 15:39:17 +0200

    Revert "14.183"
    This reverts commit 982dde36ed253b8ce3c038c9c4c73c964580d0e9.

--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -13,12 +13,12 @@ namespace Bulky.DataAccess.DbInitializer
 {
     public class DbInitializer : IDbInitializer 
     {
-            private readonly UserManager<ApplicationUser> _userManager;
+            private readonly UserManager<IdentityUser> _userManager;
             private readonly RoleManager<IdentityRole> _roleManager;
             private readonly ApplicationDbContext _db;
 
             public DbInitializer(
-                UserManager<ApplicationUser> userManager,
+                UserManager<IdentityUser> userManager,
                 RoleManager<IdentityRole> roleManager,
                 ApplicationDbContext db)
             {
