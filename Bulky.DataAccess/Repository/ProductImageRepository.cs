5051c65dc6c40803836209d8cb85e154261338fd
Francisc Suba <francisc.suba@octavic.ro>
2024-03-25 15:30:45 +0200

    16.203

---
+++ Bulky.DataAccess/Repository/ProductImageRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.DataAcces.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductImageRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(ProductImage obj)
+        {
+            _db.ProductImages.Update(obj);
+        }
+    }
+}
