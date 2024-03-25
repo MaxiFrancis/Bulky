cd45197c7f85d9f7d278c4f4aef4693a751f1a54
Francisc Suba <francisc.suba@octavic.ro>
2024-03-25 15:22:47 +0200

    16.203

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -43,6 +43,8 @@ namespace Bulky.Models
         [ForeignKey("CategoryId")]
         [ValidateNever]
         public Category Category { get; set; }
+        [ValidateNever]
+        public List<ProductImage> ProductImages { get; set; }
 
     }
 }
