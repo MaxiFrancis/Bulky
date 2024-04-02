ff76e2b26fbac657c1c50d5e1463049f5f824cb7
Francisc Suba <francisc.suba@octavic.ro>
2024-04-02 15:20:33 +0300

    Added autorization, fixed UI

--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -13,7 +13,7 @@ using Microsoft.EntityFrameworkCore;
 namespace BulkyWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    //[Authorize(Roles = SD.Role_Admin)]
+    [Authorize(Roles = SD.Role_Admin)]
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
